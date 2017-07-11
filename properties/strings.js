b7f622b4cdda43394aaf24a49995a95992b13f76
Pretorius <spretorius@wiley.com>
2017-07-11 14:06:37 +0100

    Abstract string properties into languages

--- properties/strings.js
+++ properties/strings.js
@@ -1,71 +1,8 @@
 
+import english from './languages/english';
+
 const strings = {
-
-		en : {
-			
-			denominations 		: {
-
-				btc 		: {
-					name 	: 'BitCoin' 	,
-					symbol 	: 'BTC'
-				} ,
-				usd 		: {
-					name 	: 'US Dollar' 	,
-					symbol 	: '$'
-				}
-			} ,
-
-			development : {
-
-				cache 			: 'There was an error caching assets ( see: main.js ), perhaps due to a network timeout, so we skipped caching. Reload the app to try again.' ,
-				notification 	: 'Development mode is enabled, your app will run slightly slower but you have access to useful development tools.'
-
-			} ,
-
-			errors : {
-				500 			: 'Unavailable' ,
-				ajax 			: 'There was an error retrieving the data. Please try again.' ,
-				default 		: 'Oops - an error has occurred. Please restart the application.'
-			} ,
-
-
-			screens : {
-
-				bull 			: {
-
-					changes 	: {
-						hour 	: 'Last Hour' 	,
-						day 	: 'Last Day' 	,
-						title 	: 'Movement' 	,
-						week 	: 'Last Week'
-					} ,
-
-					market 			: {
-						available 	: 'Supply Available' 	,
-						cap 		: 'Cap' 				,
-						rank 		: 'Rank' 				,
-						title 		: 'Market' 				,
-						total 		: 'Supply Total' 		,
-						updated 	: 'Last Updated' 		,
-						volume 		: '24 Hour Volume'
-					} 										,
-
-					rating 			: 'BULLet rating' 		,
-
-					title 			: 'The BULLet' 			,
-
-					values 			: {
-						title 		: 'Prices'
-					}
-
-				} ,
-
-				currencies 		: {
-					title 		:'All Crypto Currencies'
-				}
-
-			}
-		}
+		...english
 	} ,
 
 	language = 'en';
