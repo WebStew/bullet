3e2ebfab6799a1f580a883a0347541469976671b
Pretorius <spretorius@wiley.com>
2017-08-10 16:36:24 +0100

    - Completed CSS for custom tabbar bottom

--- styles/tabbar.js
+++ styles/tabbar.js
@@ -1,30 +1,28 @@
 
-//import themes from '../properties/themes';
-
 export default ( theme ) => {
 
 	return {
 
-		tabBar: {
-		height: 49, // Default tab bar height in iOS 10
-		flexDirection: 'row',
-		borderTopWidth: 1,
-		borderTopColor: theme.border,
-		backgroundColor: theme.chrome , // Default background color in iOS 10
-		},
-		tab: {
-		flex: 1,
-		alignItems: 'stretch',
-		justifyContent: 'flex-end',
-		},
-		icon: {
-		flexGrow: 1,
-		},
-		label: {
-		textAlign: 'center',
-		fontSize: 10,
-		marginBottom: 1.5,
-		backgroundColor: 'transparent',
+		tabBar 				: {
+			borderTopWidth 	: 1 			,
+			borderTopColor 	: theme.border 	,
+			backgroundColor : theme.chrome 	,
+			flexDirection 	: 'row' 		,
+			height 			: 49
+		} 									,
+		tab 				: {
+			alignItems 		: 'stretch' 	,
+			flex 			: 1 			,
+			justifyContent 	: 'flex-end'
+		} 									,
+		icon 				: {
+			flexGrow 		: 1
+		} 									,
+		label 				: {
+			backgroundColor : 'transparent' ,
+			fontSize 		: 10 			,
+			marginBottom 	: 1.5 			,
+			textAlign 		: 'center'
 		}
 	}
 
